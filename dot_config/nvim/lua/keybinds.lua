c7d3e06798babdfc71450e684e4367d02633299e
Andrew Fields <andy@andybug.com>
2023-11-19 21:18:04 -0600

    nvim: ide mode

--- dot_config/nvim/lua/keybinds.lua
+++ dot_config/nvim/lua/keybinds.lua
@@ -23,3 +23,9 @@ vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+
+-- lsp
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -12,55 +12,74 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- lazy plugins
-require("lazy").setup({
+-- always load these packages
+local packages = {
   "nvim-treesitter/nvim-treesitter",
   "Mofiqul/dracula.nvim",
-  { "folke/twilight.nvim", opts = { alpha = 0.10 } },
   {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.4",
     dependencies = { "nvim-lua/plenary.nvim" },
   },
   "nvim-tree/nvim-tree.lua",
-  "luukvbaal/nnn.nvim",
   "nvim-tree/nvim-web-devicons",
-  "sunjon/shade.nvim",
-  "lewis6991/gitsigns.nvim",
-  "neovim/nvim-lspconfig",
-  "hrsh7th/cmp-nvim-lsp",
-  "hrsh7th/cmp-buffer",
-  "hrsh7th/cmp-path",
-  "hrsh7th/cmp-cmdline",
-  "hrsh7th/nvim-cmp",
-  "L3MON4D3/LuaSnip",
-  "saadparwaiz1/cmp_luasnip",
-  "simrat39/rust-tools.nvim",
-  {
-    "nvim-neorg/neorg",
-    build = ":Neorg sync-parsers",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      require("neorg").setup({
-        load = {
-          ["core.defaults"] = {}, -- Loads default behaviour
-          ["core.concealer"] = {}, -- Adds pretty icons to your documents
-          ["core.dirman"] = { -- Manages Neorg workspaces
-            config = {
-              workspaces = {
-                notes = "~/docs/neorg",
+  "luukvbaal/nnn.nvim",
+}
+
+if vim.g.neovide then
+  local gui_packages = {
+    -- git
+    "lewis6991/gitsigns.nvim",
+    -- lsp
+    "neovim/nvim-lspconfig",
+    -- cmp
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-cmdline",
+    "hrsh7th/nvim-cmp",
+    -- snip
+    "L3MON4D3/LuaSnip",
+    "saadparwaiz1/cmp_luasnip",
+    -- neorg
+    {
+      "nvim-neorg/neorg",
+      build = ":Neorg sync-parsers",
+      dependencies = { "nvim-lua/plenary.nvim" },
+      config = function()
+        require("neorg").setup({
+          load = {
+            ["core.defaults"] = {}, -- Loads default behaviour
+            ["core.concealer"] = {}, -- Adds pretty icons to your documents
+            ["core.dirman"] = { -- Manages Neorg workspaces
+              config = {
+                workspaces = {
+                  notes = "~/docs/neorg",
+                },
               },
             },
           },
-        },
-      })
-    end,
-  },
-  {
-    "pmizio/typescript-tools.nvim",
-    dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-    opts = {},
-  },
+        })
+      end,
+    },
+    -- language-specific
+    "simrat39/rust-tools.nvim",
+    {
+      "pmizio/typescript-tools.nvim",
+      dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+      opts = {},
+    },
+  }
+
+  for _, value in ipairs(gui_packages) do
+    table.insert(packages, value)
+  end
+end
+
+-- lazy plugins
+require("lazy").setup(packages)
+  --{ "folke/twilight.nvim", opts = { alpha = 0.10 } },
+  --"sunjon/shade.nvim",
   -- {
   --   "NeogitOrg/neogit",
   --   dependencies = {
@@ -71,7 +90,6 @@ require("lazy").setup({
   --   },
   --   config = true
   -- },
-})
 
 -- dracula
 vim.cmd("colorscheme dracula")
@@ -99,79 +117,19 @@ require("nvim-treesitter.configs").setup({
 })
 
 -- shade
-require("shade").setup({
-  overlay_opacity = 60,
-  opacity_step = 1,
-  keys = {
-    -- brightness_up    = '<C-Up>',
-    -- brightness_down  = '<C-Down>',
-    toggle = "<leader>s",
-  },
-})
-
--- gitsigns
-require("gitsigns").setup()
-
--- nvim-cmp
-local cmp = require("cmp")
-
-cmp.setup({
-  snippet = {
-    -- REQUIRED - you must specify a snippet engine
-    expand = function(args)
-      -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-      require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
-      -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-      -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-    end,
-  },
-  window = {
-    completion = cmp.config.window.bordered(),
-    documentation = cmp.config.window.bordered(),
-  },
-  mapping = cmp.mapping.preset.insert({
-    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-    ["<C-f>"] = cmp.mapping.scroll_docs(4),
-    ["<C-Space>"] = cmp.mapping.complete(),
-    ["<C-e>"] = cmp.mapping.abort(),
-    ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-  }),
-  sources = cmp.config.sources({
-    { name = "nvim_lsp" },
-    -- { name = 'vsnip' }, -- For vsnip users.
-    { name = "luasnip" }, -- For luasnip users.
-    -- { name = 'ultisnips' }, -- For ultisnips users.
-    -- { name = 'snippy' }, -- For snippy users.
-  }, {
-    { name = "buffer" },
-  }),
-})
-
--- TODO: remove
--- local capabilities = require("cmp_nvim_lsp").default_capabilities()
---
--- -- lspconfig
--- local lspconfig = require("lspconfig")
--- lspconfig.rust_analyzer.setup({
---   capabilities = capabilities,
+-- require("shade").setup({
+--   overlay_opacity = 60,
+--   opacity_step = 1,
+--   keys = {
+--     -- brightness_up    = '<C-Up>',
+--     -- brightness_down  = '<C-Down>',
+--     toggle = "<leader>s",
+--   },
 -- })
 
+-- nvim-tree
 require("nvim-tree").setup()
-
--- rust
-local rt = require("rust-tools")
-
-rt.setup({
-  server = {
-    on_attach = function(_, bufnr)
-      -- Hover actions
-      vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
-      -- Code action groups
-      vim.keymap.set("n", "<leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
-    end,
-  },
-})
-
+  
 --- nnn
 require("nnn").setup({
   explorer = {
@@ -182,17 +140,84 @@ require("nnn").setup({
   },
 })
 
--- typescript
-require("typescript-tools").setup {
-  settings = {
-    tsserver_file_preferences = {
-      includeInlayParameterNameHints = "all",
-      includeCompletionsForModuleExports = true,
-      quotePreference = "auto",
+
+if vim.g.neovide then
+
+  -- gitsigns
+  require("gitsigns").setup()
+
+  -- nvim-cmp
+  local cmp = require("cmp")
+  
+  cmp.setup({
+    snippet = {
+      -- REQUIRED - you must specify a snippet engine
+      expand = function(args)
+        -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+        require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+        -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+        -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+      end,
     },
-    -- tsserver_format_options = {
-    --   allowIncompleteCompletions = false,
-    --   allowRenameOfImportPath = false,
-    -- }
-  },
-}
+    window = {
+      completion = cmp.config.window.bordered(),
+      documentation = cmp.config.window.bordered(),
+    },
+    mapping = cmp.mapping.preset.insert({
+      ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+      ["<C-f>"] = cmp.mapping.scroll_docs(4),
+      ["<C-Space>"] = cmp.mapping.complete(),
+      ["<C-e>"] = cmp.mapping.abort(),
+      ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    }),
+    sources = cmp.config.sources({
+      { name = "nvim_lsp" },
+      -- { name = 'vsnip' }, -- For vsnip users.
+      { name = "luasnip" }, -- For luasnip users.
+      -- { name = 'ultisnips' }, -- For ultisnips users.
+      -- { name = 'snippy' }, -- For snippy users.
+    }, {
+      { name = "buffer" },
+    }),
+  })
+  
+  -- TODO: remove
+  -- local capabilities = require("cmp_nvim_lsp").default_capabilities()
+  --
+  -- -- lspconfig
+  -- local lspconfig = require("lspconfig")
+  -- lspconfig.rust_analyzer.setup({
+  --   capabilities = capabilities,
+  -- })
+  
+  
+  -- rust
+  local rt = require("rust-tools")
+  
+  rt.setup({
+    server = {
+      on_attach = function(_, bufnr)
+        -- Hover actions
+        vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+        -- Code action groups
+        vim.keymap.set("n", "<leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+      end,
+    },
+  })
+  
+  -- typescript
+  require("typescript-tools").setup {
+    settings = {
+      tsserver_file_preferences = {
+        includeInlayParameterNameHints = "all",
+        includeCompletionsForModuleExports = true,
+        quotePreference = "auto",
+      },
+      -- tsserver_format_options = {
+      --   allowIncompleteCompletions = false,
+      --   allowRenameOfImportPath = false,
+      -- }
+    },
+  }
+
+end
