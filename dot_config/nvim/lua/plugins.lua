dde29041c1755023d0c110def6311dd58bbab3c3
Andrew Fields <andy@andybug.com>
2023-11-27 21:17:22 -0600

    nvim: add border to LSP windows

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -189,8 +189,30 @@ if vim.g.neovide then
   -- lspconfig.rust_analyzer.setup({
   --   capabilities = capabilities,
   -- })
-  
-  
+
+  -- lsp
+  local _border = "single"
+
+  vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+    vim.lsp.handlers.hover, {
+      border = _border
+    }
+  )
+
+  vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
+    vim.lsp.handlers.signature_help, {
+      border = _border
+    }
+  )
+
+  vim.diagnostic.config{
+    float={border=_border}
+  }
+
+  require('lspconfig.ui.windows').default_options = {
+    border = _border
+  }
+
   -- rust
   local rt = require("rust-tools")
   
